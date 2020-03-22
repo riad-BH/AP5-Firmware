d997aebd884720b13f28c357bae2062f4351f4b2
riad-BH <riadbenhalla@gmail.com>
2020-03-23 00:05:43 +0100

    #A Timer 2 and 4

--- src/TimerSetting.h
+++ src/TimerSetting.h
@@ -1,7 +1,12 @@
 #ifndef H_TIMER1_SETTING
 #define H_TIMER1_SETTING
 #include <Arduino.h>
-// Setting timer1 to give an interruption in micro-seconds
+// Setting timer1 to give an interruption in micro-seconds for X Y axis motors pulses
 void setTimer1(uint16_t);
+// Setting timer2 for the temperature control ISR
+void setTimer2();
+// Setting timer3 to give an interruption in micro-seconds for Z E axismotors pulses
 void setTimer3(uint16_t);
+// Setting timer4 for PWM signal for the mosfet (Temperature control) on PIN 6
+void setTimer4();
 #endif // H_TIMER1_SETTING
